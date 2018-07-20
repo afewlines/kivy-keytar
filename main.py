cc0fe54593080bcb82b4014f7c0f57b6df8c4a25
afewlines <jato547@gmail.com>
2018-07-20 02:13:40 -0400

    enter the jacob era. if you out there, bless you

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+from random import randint
+
 from kivy.app import App
-from kivy.config import Config
 from kivy.clock import Clock
+from kivy.config import Config
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 from kivy.uix.image import Image
-
-# non kivy imports
-from random import randint
+from kivy.uix.label import Label
 
 
 class Board():
@@ -32,10 +31,10 @@ class GameScreen(BoxLayout):
     active = False
     tiles = ['graphics/tile0.png',
              'graphics/tile1.png']
-    keyBindings = {'a': 0, 's': 1, 'k': 2, 'l': 3}
+    key_binds = {'a': 0, 's': 1, 'k': 2, 'l': 3}
     board = []
-    screenTiles = [[], [], [], []]
-    currentRow = 0
+    screen_tiles = [[], [], [], []]
+    current_row = 0
     ttime = 0
 
     def __init__(self, **kwargs):
@@ -54,10 +53,10 @@ class GameScreen(BoxLayout):
             text='press \'r\' or \'spacebar\' to start', font_size=24, size_hint_max_y=400, halign='center')
         self._scrCurrent = self._scrReset
 
-        self.timerLabel = Label(
+        self.timer_label = Label(
             text=str(GameScreen.ttime), text_size=(150, None))
-        self.header.add_widget(self.timerLabel)
-        self.level = Label(text=str(GameScreen.currentRow),
+        self.header.add_widget(self.timer_label)
+        self.level = Label(text=str(GameScreen.current_row),
                            text_size=(150, None), halign='right')
         self.header.add_widget(self.level)
 
@@ -76,40 +75,40 @@ class GameScreen(BoxLayout):
             if keycode[1] in ['r', 'spacebar']:
                 self.reset()
             return
-        if keycode[1] in GameScreen.keyBindings:
-            if GameScreen.board[GameScreen.currentRow][GameScreen.keyBindings[keycode[1]]] == 1:
-                GameScreen.currentRow += 1
-                self.updateScreen()
+        if keycode[1] in GameScreen.key_binds:
+            if GameScreen.board[GameScreen.current_row][GameScreen.key_binds[keycode[1]]] == 1:
+                GameScreen.current_row += 1
+                self.update_screen()
             else:
                 self.fail()
 
     def timer(self, dt):
         if GameScreen.active:
             GameScreen.ttime += dt
-            self.timerLabel.text = '{0:.3f}'.format(GameScreen.ttime)
+            self.timer_label.text = '{0:.3f}'.format(GameScreen.ttime)
         else:
             return False
 
-    def updateScreen(self):
-        self.level.text = str(GameScreen.currentRow)
-        if GameScreen.currentRow >= 50:
+    def update_screen(self):
+        self.level.text = str(GameScreen.current_row)
+        if GameScreen.current_row >= 50:
             self.win()
             return
         for row in range(0, 4):
-            GameScreen.screenTiles[row] = GameScreen.board[row +
-                                                           GameScreen.currentRow]
+            GameScreen.screen_tiles[row] = GameScreen.board[row +
+                                                            GameScreen.current_row]
         self.grid.clear_widgets()
         for row in range(0, 4):
             for tile in range(0, 4):
                 self.grid.add_widget(
-                    Image(source=GameScreen.tiles[GameScreen.screenTiles[3 - row][tile]]))
+                    Image(source=GameScreen.tiles[GameScreen.screen_tiles[3 - row][tile]]))
 
     def fail(self):
         print('r i p my dude')
         GameScreen.active = False
         self.grid.clear_widgets()
         self.remove_widget(self.grid)
-        self._scrReset.text = 'you lost :(\npress \'r\' or \'spacebar\' to restart'
+        self._scrReset.text = "you lost :(\npress 'r' or 'spacebar' to restart"
         self._scrCurrent = self._scrReset
         self.add_widget(self._scrCurrent)
 
@@ -118,19 +117,19 @@ class GameScreen(BoxLayout):
         GameScreen.active = False
         self.grid.clear_widgets()
         self.remove_widget(self.grid)
-        self._scrWin.text = 'you won!\ntime: ' + self.timerLabel.text + \
-            '\npress \'r\' or \'spacebar\' to restart'
+        self._scrWin.text = "you won!\ntime: {}\npress 'r' or 'spacebar' to restart".format(
+            self.timer_label.text)
         self._scrCurrent = self._scrWin
         self.add_widget(self._scrCurrent)
 
     def reset(self):
         GameScreen.active = True
-        GameScreen.currentRow = 0
+        GameScreen.current_row = 0
         GameScreen.ttime = 0
         GameScreen.board = Board(50).getBoard()
         self.remove_widget(self._scrCurrent)
         self.add_widget(self.grid)
-        self.updateScreen()
+        self.update_screen()
         Clock.schedule_interval(self.timer, 0.01)
 
 
@@ -141,9 +140,9 @@ class KeytarApp(App):
 
 if __name__ == '__main__':
     # Config.set('graphics', 'borderless', '1')
-    # Config.set('graphics', 'width', '400')
-    # Config.set('graphics', 'height', '630')
+    Config.set('graphics', 'width', '400')
+    Config.set('graphics', 'height', '630')
     # Config.set('graphics', 'resizable', '0')
-    # Config.write()
+    Config.write()
     KeytarApp().run()
     print('gameOVER')
